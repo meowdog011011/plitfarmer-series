59c8515e47c28a45eb4dc9ff9a067bc4caf1ac0b
Te Du <tdu0915@gmail.com>
2023-08-24 12:11:10 -0400

    A whole lotta updates
    WebGL v 1.1.1, v 1.1.5, v1.1.10, update README... Whew!

--- README.md
+++ README.md
@@ -18,3 +18,13 @@ If you want to contribute with your ideas, create an issue. All ideas and feedba
 8/10/2023 11:20 AM - v 1.1.0 added some more enhancements and a pause option.
 
 8/10/2023 2:01 PM - v 1.1.1 fixed Shooting Enemy's bug. Mistakenly believed to be resolved in v 1.1.0, but now I think it is truly fixed.
+
+8/14/2023 3:31 PM - v 1.1.5 changed pause button's look and optimized code. Also fixed [this issue](https://github.com/meowdog011011/first-unity-game/issues/2) and added Back button to pause screen.
+
+8/24/2023 11:55 PM - v 1.1.10 added **Liberation Sans** to the credits and did a number of other changes:
+- changed pause behaviour with entities
+- changed hitboxes of spike and enemies
+- Shooting Enemies will now only spawn at lvl. 5 or higher
+- blinking player!
+- added new level button when you lose
+- special effect when you win
---
+++ v 1.1.10/Assets/MovingEnemyScript.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingEnemyScript : MonoBehaviour
+{
+    [SerializeField] private new Rigidbody2D rigidbody;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite playerNormal;
+    [SerializeField] private Sprite playerBlinking;
+    private GameScript gameScript;
+    public float x = 2;
+    public float speed;
+    private Vector3 pausePosition;
+    private Vector2 pauseRigidbodyVelocity;
+
+    void Start()
+    {
+        gameScript = GameObject.Find("Game Manager").GetComponent<GameScript>();
+        StartCoroutine(Blink());
+    }
+
+    void Update()
+    {
+        if (gameScript.stage != 5)
+        {
+            pausePosition = new Vector3(0, 0, 0);
+            pauseRigidbodyVelocity = new Vector2(0, 0);
+        }
+        else
+        {
+            if (pausePosition == new Vector3(0, 0, 0))
+            {
+                pausePosition = transform.position;
+            }
+            if (pauseRigidbodyVelocity == new Vector2(0, 0))
+            {
+                pauseRigidbodyVelocity = rigidbody.velocity;
+            }
+            transform.position = pausePosition;
+            rigidbody.velocity = pauseRigidbodyVelocity;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (gameScript.stage != 0 & gameScript.stage != 4 & gameScript.stage != 5)
+        {
+            if (x == -2)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else
+            {
+                spriteRenderer.flipX = false;
+            }
+            rigidbody.velocity = new Vector3(x, 0, 0) * speed * Time.fixedDeltaTime;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name != "Left Platform" & collision.gameObject.name != "Right Platform")
+        {
+            x *= -1;
+        }
+    }
+
+    IEnumerator Blink()
+    {
+        for (int i = 0; i < 1; i += 0)
+        {
+            if (gameScript.stage != 5)
+            {
+                spriteRenderer.sprite = playerNormal;
+            }
+            yield return new WaitForSeconds(1.5f);
+            if (gameScript.stage != 5)
+            {
+                spriteRenderer.sprite = playerBlinking;
+            }
+            yield return new WaitForSeconds(0.1f);
+            if (gameScript.stage != 5)
+            {
+                spriteRenderer.sprite = playerNormal;
+            }
+            yield return new WaitForSeconds(0.5f);
+            if (gameScript.stage != 5)
+            {
+                spriteRenderer.sprite = playerBlinking;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
